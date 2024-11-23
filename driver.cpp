2393aa105a8aadde053f182b79543a9fb5b88ab8
fornerobj <fornerobj@gmail.com>
2024-11-23 15:21:11 -0600

    Updated output for easier readabilty and usability

--- driver.cpp
+++ driver.cpp
@@ -3,29 +3,25 @@
 #include "DPLL.h"
 
 int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <input file> -[additional literals]" << std::endl;
-        return -1;
-    }
 
-    for (int i = 0; i < argc; ++i) {
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
+    const std::string filename = "./sudoku.cnf";
 
-    const std::string filename = argv[1];
+    std::cout << "Enter the starting layout of the puzzle, one row at a time" << std::endl;
+    std::cout << "Use 0 for empty spaces" << std::endl;
 
-    // Collect additional literals if provided
     std::vector<std::string> extraLiterals;
     std::string line; 
+
     int row = 1;
     for(int i = 0; i < 9; i++) {
         std::cin >> line;
+
         if(line.size() != 9) {
             std::cout << "Invalid line length (must be 9)" << std::endl;
             i--;
             continue;
         }
+
         bool allNums = true;
         for (char c : line) {
             if(!isdigit(c)) {
@@ -38,8 +34,8 @@ int main(int argc, char* argv[]) {
             i--;
             continue;
         }
-        std::stringstream ss;
 
+        std::stringstream ss;
         int col = 1;
         for (char c : line) {
             if(c == '0'){
@@ -54,22 +50,41 @@ int main(int argc, char* argv[]) {
         row++;
 
     }
-    for(std::string str : extraLiterals) {
-        std::cout << str << std::endl;
-    }
+
     DPLL problem(filename, extraLiterals);
-    if(problem.search({}, problem.literals)) {
-        std::cout << "Solution found!" << std::endl;
-        std::cout << "Just the true propositions:" << std::endl;
-        for(auto mapping : problem.model) {
-            if(mapping.second){
-                std::cout << problem.reverseLiteralMap[mapping.first] << std::endl;
-            }
-        }
-        std::cout << "\n";
-    } else {
+    std::cout << "Thinking..." << std::endl;
+    if(!problem.search({}, problem.literals)) {
         std::cout << "No solution found :(" << std::endl;
+        return -1;
     }
 
-    std::cout << "Number of DPLL calls: " << problem.numCalls << std::endl;
+    std::cout << "Solution found!\n" << std::endl;
+
+    // std::vector<std::string> solution;
+    char** solution = new char*[9];
+    for(int i = 0; i < 9; i++) {
+        solution[i] = new char[9];
+    }
+
+    for(auto mapping : problem.model) {
+        if(mapping.second){
+            std::string literal = problem.reverseLiteralMap[mapping.first];
+            int row = (int)literal[1] - '0' - 1;
+            int col = (int)literal[2] - '0' - 1;
+            solution[row][col] = literal[3];
+        }
+    }
+
+    for(int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            std::cout << solution[i][j];
+            if(j == 2 || j == 5) {
+                std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+        if(i == 2 || i == 5) {
+            std::cout << std::endl;
+        }
+    }
  }
